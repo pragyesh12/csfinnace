c0952bf242bfb1d80c75338b8d717e72d2428665
Pragyesh singh <38255620+pragyesh12@users.noreply.github.com>
2018-10-28 23:59:35 +0530

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -1,1 +1,16 @@
+<?php
+?>
+<html>
+<body>
+<h1>csfinnace</h1>
+<br>
+<br>
+<br>
+<a href="login.php">login</a><br>
+<a href="register.php">register</a><br>
+<a href="index.php">getquotes</a><br>
+<a href="sellquotes.php">sellquotes</a><br>
+<a href="buystock.php">buystock</a><br>
+</body>
+</html>
 
